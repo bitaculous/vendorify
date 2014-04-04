37a945ec715c81445580480482371b74b78dfd7e
Maik Kempe <mkempe@nrolla.com>
2014-04-04 16:39:13 +0200

    [➠] Improved specs (“SassIntegration”).

--- spec/vendorify/integrations/sass_integration_spec.rb
+++ spec/vendorify/integrations/sass_integration_spec.rb
@@ -5,21 +5,23 @@ describe Vendorify::Integrations::SassIntegration do
 
   let(:load_paths) { ::Sass.load_paths }
 
-  context 'the “vendor stylesheets” path exists' do
-    it 'expands the load path' do
-      Vendorify::Integrations::SassIntegration.setup(vendor_stylesheets_path: vendor_stylesheets_path)
+  describe '“vendor stylesheets””' do
+    context 'the “vendor stylesheets” path exists' do
+      it 'expands the load path' do
+        Vendorify::Integrations::SassIntegration.setup vendor_stylesheets_path: vendor_stylesheets_path
 
-      expect(load_paths).to include vendor_stylesheets_path
+        expect(load_paths).to include vendor_stylesheets_path
+      end
     end
-  end
 
-  context 'the “vendor stylesheets” path does not exists' do
-    let(:vendor_stylesheets_path) { '/foo' }
+    context 'the “vendor stylesheets” path does not exists' do
+      let(:vendor_stylesheets_path) { '/foo' }
 
-    it 'does not expands the load path' do
-      Vendorify::Integrations::SassIntegration.setup(vendor_stylesheets_path: vendor_stylesheets_path)
+      it 'does not expands the load path' do
+        Vendorify::Integrations::SassIntegration.setup vendor_stylesheets_path: vendor_stylesheets_path
 
-      expect(load_paths).not_to include vendor_stylesheets_path
+        expect(load_paths).not_to include vendor_stylesheets_path
+      end
     end
   end
 end
