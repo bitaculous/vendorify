e1f8b9eaad43544d9c213647106dec46d0b50281
Maik Kempe <mkempe@nrolla.com>
2014-04-04 16:41:52 +0200

    [➠] Don't expand load path if path does not exists (“SprocketsIntegration”). [➠] Updated TODOS.

--- lib/vendorify/integrations/sprockets_integration.rb
+++ lib/vendorify/integrations/sprockets_integration.rb
@@ -3,11 +3,14 @@ require 'sprockets'
 module Vendorify
   module Integrations
     class SprocketsIntegration
-      def self.setup
-        ::Sprockets.append_path Vendorify.vendor_fonts_path
-        ::Sprockets.append_path Vendorify.vendor_images_path
-        ::Sprockets.append_path Vendorify.vendor_javascripts_path
-        ::Sprockets.append_path Vendorify.vendor_stylesheets_path
+      def self.setup(vendor_fonts_path: Vendorify.vendor_fonts_path,
+                     vendor_images_path: Vendorify.vendor_images_path,
+                     vendor_javascripts_path: Vendorify.vendor_javascripts_path,
+                     vendor_stylesheets_path: Vendorify.vendor_stylesheets_path)
+        ::Sprockets.append_path vendor_fonts_path       if File.directory? vendor_fonts_path
+        ::Sprockets.append_path vendor_images_path      if File.directory? vendor_images_path
+        ::Sprockets.append_path vendor_javascripts_path if File.directory? vendor_javascripts_path
+        ::Sprockets.append_path vendor_stylesheets_path if File.directory? vendor_stylesheets_path
       end
     end
   end
--- spec/vendorify/integrations/sprockets_integration_spec.rb
+++ spec/vendorify/integrations/sprockets_integration_spec.rb
@@ -1,12 +1,87 @@
 require 'spec_helper'
 
 describe Vendorify::Integrations::SprocketsIntegration do
+  include Helpers
+
   let(:load_paths) { Sprockets.paths }
 
-  it 'expands the load path' do
-    expect(load_paths).to include Vendorify.vendor_fonts_path
-    expect(load_paths).to include Vendorify.vendor_images_path
-    expect(load_paths).to include Vendorify.vendor_javascripts_path
-    expect(load_paths).to include Vendorify.vendor_stylesheets_path
+  describe '“vendor fonts”' do
+    context 'the “vendor fonts” path exists' do
+      it 'expands the load path' do
+        Vendorify::Integrations::SprocketsIntegration.setup vendor_fonts_path: vendor_fonts_path
+
+        expect(load_paths).to include vendor_fonts_path
+      end
+    end
+
+    context 'the “vendor fonts” path does not exists' do
+      let(:vendor_fonts_path) { '/foo' }
+
+      it 'does not expands the load path' do
+        Vendorify::Integrations::SprocketsIntegration.setup vendor_fonts_path: vendor_fonts_path
+
+        expect(load_paths).not_to include vendor_fonts_path
+      end
+    end
+  end
+
+  describe '“vendor images”' do
+    context 'the “vendor images” path exists' do
+      it 'expands the load path' do
+        Vendorify::Integrations::SprocketsIntegration.setup vendor_images_path: vendor_images_path
+
+        expect(load_paths).to include vendor_images_path
+      end
+    end
+
+    context 'the “vendor images” path does not exists' do
+      let(:vendor_images_path) { '/foo' }
+
+      it 'does not expands the load path' do
+        Vendorify::Integrations::SprocketsIntegration.setup vendor_images_path: vendor_images_path
+
+        expect(load_paths).not_to include vendor_images_path
+      end
+    end
+  end
+
+  describe '“vendor javascripts”' do
+    context 'the “vendor javascripts” path exists' do
+      it 'expands the load path' do
+        Vendorify::Integrations::SprocketsIntegration.setup vendor_javascripts_path: vendor_javascripts_path
+
+        expect(load_paths).to include vendor_javascripts_path
+      end
+    end
+
+    context 'the “vendor javascripts” path does not exists' do
+      let(:vendor_javascripts_path) { '/foo' }
+
+      it 'does not expands the load path' do
+        Vendorify::Integrations::SprocketsIntegration.setup vendor_javascripts_path: vendor_javascripts_path
+
+        expect(load_paths).not_to include vendor_javascripts_path
+      end
+    end
+  end
+
+  describe '“vendor stylesheets”' do
+    context 'the “vendor stylesheets” path exists' do
+      it 'expands the load path' do
+        Vendorify::Integrations::SprocketsIntegration.setup vendor_stylesheets_path: vendor_stylesheets_path
+
+        expect(load_paths).to include vendor_stylesheets_path
+      end
+    end
+
+    context 'the “vendor stylesheets” path does not exists' do
+      let(:vendor_stylesheets_path) { '/foo' }
+
+      it 'does not expands the load path' do
+        Vendorify::Integrations::SprocketsIntegration.setup vendor_stylesheets_path: vendor_stylesheets_path
+
+        expect(load_paths).not_to include vendor_stylesheets_path
+      end
+    end
   end
 end
